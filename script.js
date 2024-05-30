2d418ea674a09e987fd92416f922f0cfa4744efc
uriah <108307211+IronAntelope@users.noreply.github.com>
2024-05-30 14:34:40 +0930

    Add operate function
    declared variables for 2 numbers and an operator.
    added operate function which calls basic logic functions on vars

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ function add(a, b) {return a + b};
 function subtract(a, b) {return a - b};
 function multiply(a, b) {return a * b};
 function divide(a, b) {return a / b};
+
+// declare vars
+let numA;
+let numB;
+let operator;
+
+function operate(numA, numB, operator) {
+  switch(operator) {
+    case "add":
+      return add(numA, numB);
+      break;
+    case "subtract":
+      return subtract(numA, numB);
+      break;
+    case "multiply":
+      return multiply(numA, numB);
+      break;
+    case "divide":
+      return divide(numA, numB);
+      break;
+  };
+};
